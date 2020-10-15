382e0fbed66689f398d245a72561e9105e37d133
Walter White <WalterWhite@walterjwhite.com>
2020-10-15 07:40:10 -0400

    merged

--- lib/project.directory.sh
+++ lib/project.directory.sh
@@ -3,17 +3,29 @@
 .  _LIBRARY_PATH_/_APPLICATION_NAME_/include.sh
 .  _LIBRARY_PATH_/install/logging.sh
 
+_get_project_relative_path() {
+	echo $1 | sed -e "s/^.*@//" | sed -e "s/\:\//\//g"
+}
+
+_get_local_relative_path() {
+	local _length=${#_PROJECT_BASE_PATH}
+	_length=$((_length + 2))
+
+	echo $1 | cut -c $_length-
+}
+
 _get_project_directory() {
-	_in=$(pwd | grep -c $HOME)
-	#_in=$(pwd | grep -c $_PROJECT_BASE_PATH)
+	#_in=$(pwd | grep -c $HOME)
+	_in=$(pwd | grep -c $_PROJECT_BASE_PATH)
 	if [ "$_in" -eq "0" ]
 	then
-		exitWithError "Outside $HOME directory, unable to find project directory" 1
+		exitWithError "Outside $_PROJECT_BASE_PATH directory, unable to find project directory @  $(pwd)" 1
 	fi
 
 	if [ -e .git ]
 	then
 		_PROJECT_PATH=$(pwd)
+		_PROJECT_RELATIVE_PATH=$(_get_local_relative_path $_PROJECT_PATH)
 		_PROJECT=$(basename $_PROJECT_PATH)
 
 		return
