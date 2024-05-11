53d18a31edf44b1be4927a8c320be6e78bbac82f
Walter White <WalterWhite@walterjwhite.com>
2024-05-11 07:37:10 -0400

    default - merged

---
+++ all/lib/data.app.sh
@@ -0,0 +1,7 @@
+import include.sh
+
+_PROJECT_PATH=$_CONF_INSTALL_APPLICATION_DATA_PATH
+
+_SYSTEM=$(head -1 /usr/local/etc/walterjwhite/system 2>/dev/null)
+_PROJECT=data/$_SYSTEM/$USER/$_APPLICATION_NAME
+
